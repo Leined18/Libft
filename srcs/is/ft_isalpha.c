6e9f60a6d77e65928b255ebe5721218ff1bf1f7a
Leined <erdanielmarciano@hotmail.com>
2024-06-10 18:19:16 +0200

    Remaked Makefile. maked dir per typefuntion.

--- srcs/is/ft_isalpha.c
+++ srcs/is/ft_isalpha.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danpalac <danpalac@student.42madrid>       +#+  +:+       +#+        */
+/*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 13:14:37 by danpalac          #+#    #+#             */
-/*   Updated: 2024/02/08 11:37:38 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/06/10 18:15:02 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	ft_isalpha(int c)
 	else
 		return (0);
 }
+
 /*
 int main()
 {
