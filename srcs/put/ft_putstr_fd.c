dc149d100eb6792fb564a9cf7c2d020d676ad450
Leined18 <erdanielmarciano@hotmail.com>
2024-10-05 22:57:34 +0200

    norminette fixed

--- srcs/put/ft_putstr_fd.c
+++ srcs/put/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 09:02:49 by danpalac          #+#    #+#             */
-/*   Updated: 2024/10/05 12:17:12 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/10/05 22:19:32 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-    int    i;
-    
-    i = 0;
+	int	i;
+
+	i = 0;
 	while (s[i])
 		ft_putchar_fd(s[i++], fd);
 }
 /*
-int main() {
-    // Write C code here
-    char c[] = "Piano";
-    int i = 0;
+int	main(void) {
+	// Write C code here
+	char c[] = "Piano";
+	int i = 0;
 
-    while (i < sizeof(int))
-    {
-        ft_putstr_fd(c,1);
-        ft_putchar_fd(' ', 1);
-        i++;
-    }
+	while (i < sizeof(int))
+	{
+		ft_putstr_fd(c,1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
 
-    return 0;
+	return (0);
 }*/
