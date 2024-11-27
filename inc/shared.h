53fc9c23ecad305dadd0dcc98bb53a5fa0cfbf31
Leined18 <erdanielmarciano@hotmail.com>
2024-11-27 17:21:21 +0100

    added colours

--- inc/shared.h
+++ inc/shared.h
@@ -26,6 +26,30 @@
 # define WHITE "\033[0;37m"
 # define RESET "\033[0m"
 
+# define BOLD_BLACK    "\033[1;30m"
+# define BOLD_RED      "\033[1;31m"
+# define BOLD_GREEN    "\033[1;32m"
+# define BOLD_YELLOW   "\033[1;33m"
+# define BOLD_BLUE     "\033[1;34m"
+# define BOLD_MAGENTA  "\033[1;35m"
+# define BOLD_CYAN     "\033[1;36m"
+# define BOLD_WHITE    "\033[1;37m"
+
+# define ORANGE        "\033[38;5;208m"
+# define WINE          "\033[38;5;88m"
+# define LIME          "\033[38;5;190m"
+# define TURQUOISE     "\033[38;5;38m"
+# define LIGHT_PINK    "\033[38;5;13m"
+# define DARK_GRAY     "\033[38;5;235m"
+# define LIGHT_RED     "\033[38;5;203m"
+# define LIGHT_BLUE    "\033[38;5;75m"
+
+# define BRIGHT_BLUE   "\033[38;5;27m"
+# define BRIGHT_GREEN  "\033[38;5;46m"
+# define BRIGHT_YELLOW  "\033[38;5;226m"
+# define BRIGHT_CYAN   "\033[38;5;51m"
+# define BRIGHT_WHITE  "\033[38;5;231m"
+
 # define TABLE_SIZE 1000003
 
 #endif // SHARDED_H
