2b93b06e145034e3c959275367f6adf50956704b
danpalac <daps2005@hotmail.com>
2025-06-05 13:32:03 +0200

    ft_realloc modified

--- srcs/mem/ft_realloc.c
+++ srcs/mem/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 19:33:21 by daniel            #+#    #+#             */
-/*   Updated: 2025/06/05 10:04:39 by danpalac         ###   ########.fr       */
+/*   Updated: 2025/06/05 13:16:25 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
     if (new_size == 0)
         return (free(ptr), NULL);
     if (ptr == NULL)
-        return (malloc(new_size));
+    {
+        new_ptr = malloc(new_size);
+        if (!new_ptr)
+            return (NULL);
+        memset(new_ptr, 0, new_size);
+        return (new_ptr);
+    }
     if (old_size == new_size)
         return (ptr);
     new_ptr = malloc(new_size);
